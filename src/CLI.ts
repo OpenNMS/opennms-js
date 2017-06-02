93c8778ab7269b1879225c7711b4f1513a6dc8be
Benjamin Reed <ranger@opennms.org>
2017-06-02 10:11:11 -0400

    feat(cli): add server info display

--- src/CLI.ts
+++ src/CLI.ts
@@ -1,35 +1,138 @@
+import * as startCase from 'lodash.startcase';
+
 import {Client} from './Client';
 
 import {OnmsAuthConfig} from './api/OnmsAuthConfig';
 import {OnmsServer} from './model/OnmsServer';
+import {ServerType} from './api/Constants';
 
 import {AxiosHTTP} from './rest/AxiosHTTP';
 import {SuperAgentHTTP} from './rest/SuperAgentHTTP';
 
-import {factory} from './api/Log';
+import {log, catRoot, setLogLevel} from './api/Log';
+import {Category,
+  CategoryServiceFactory,
+  CategoryDefaultConfiguration,
+  LogLevel} from 'typescript-logging';
 
-// tslint:disable-next-line
-let program = require('commander');
+const catCLI = new Category('cli', catRoot);
 
-const log = factory.getLogger('CLI');
+// tslint:disable
+const cliff = require('cliff');
+const colors = require('colors');
+const fs = require('fs');
+const path = require('path');
+const program = require('commander');
+// tslint:enable
 
-const cli = (url) => {
-  const server = new OnmsServer('OpenNMS', url, new OnmsAuthConfig(program.username, program.password));
-  const http = new AxiosHTTP(server);
-  Client.checkServer(server, http).then((res) => {
-    log.info(res.data);
-    return res;
-  }).catch((err) => {
-    if (err.stack) {
-      log.error(err.stack);
-    }
-    return err;
-  });
-};
+const homedir = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+const defaultConfigFile = path.join(homedir, '.opennms-cli.config.json');
 
+function readConfig() {
+  const configfile = program.config || defaultConfigFile;
+  let config;
+  if (fs.exists) {
+    config = JSON.parse(fs.readFileSync(configfile));
+  } else {
+    config = {
+      password: undefined,
+      url: undefined,
+      username: undefined,
+    };
+  }
+  return config;
+}
+
+/* tslint:disable:no-console */
+
+// global options
 program
-  .arguments('<url>')
-  .option('-u, --username <username>', 'The user to authenticate as')
-  .option('-p, --password <password>', 'The password to authenticate with')
-  .action(cli)
-  .parse(process.argv);
+  .option('-d, --debug', 'Enable debug output', () => {
+    setLogLevel(LogLevel.Debug);
+  })
+  .option('-c, --config <file>', 'Specify a configuration file (default: ~/.opennms-cli.config');
+
+// connect (validate server and save config)
+program
+  .command('connect [url]')
+  .description('Connect to an OpenNMS Horizon or Meridian server')
+  .option('-u, --username <username>', 'The username to authenticate as (default: admin)')
+  .option('-p, --password <password>', 'The password to authenticate with (default: admin)')
+  .action((url, options) => {
+    const config = readConfig();
+    if (url) {
+      config.url = url;
+    }
+    if (options.username) {
+      config.username = options.username;
+    }
+    if (options.password) {
+      config.password = options.password;
+    }
+    const server = new OnmsServer('OpenNMS', config.url, new OnmsAuthConfig(config.username, config.password));
+    const http = new AxiosHTTP(server);
+    return Client.checkServer(server, http).then(() => {
+      console.log(colors.green('Connection succeeded.'));
+      if (!program.config) { // don't write the config if a config was passed in
+        log.debug('Saving configuration to ' + defaultConfigFile, catCLI);
+        fs.writeFileSync(defaultConfigFile, JSON.stringify(config, undefined, 2), { mode: 0o600 });
+      }
+      /*
+      if (res.data.type === ServerType.MERIDIAN) {
+        console.log(colors.blue('Connected to OpenNMS Meridian ' + res.data.version.displayVersion));
+      } else {
+        console.log(colors.green('Connected to OpenNMS Horizon ' + res.data.version.displayVersion));
+      }
+      */
+      return true;
+    }).catch((err) => {
+      if (err.stack) {
+        log.error(err.stack, err, catCLI);
+      }
+      return err;
+    });
+  });
+
+// test another option
+program
+  .command('capabilities')
+  .description('List the API capabilities of the OpenNMS server')
+  .action(() => {
+    const config = readConfig();
+    const server = new OnmsServer('OpenNMS', config.url, new OnmsAuthConfig(config.username, config.password));
+    const http = new AxiosHTTP(server);
+    return Client.getMetadata(server, http).then((res) => {
+      let c = colors.green;
+      if (res.data.type === ServerType.MERIDIAN) {
+        console.log(colors.blue('OpenNMS Meridian ' + res.data.version.displayVersion + ' Capabilities:'));
+        c = colors.blue;
+      } else {
+        console.log(colors.green('OpenNMS Horizon ' + res.data.version.displayVersion + ' Capabilities:'));
+      }
+      console.log('');
+
+      const caps = res.data.capabilities();
+      const rows = [];
+      for (const cap in caps) {
+        if (cap === 'type') {
+          continue;
+        }
+        rows.push([startCase(cap), caps[cap]]);
+      }
+      console.log(cliff.stringifyRows(rows));
+      console.log('');
+
+      return res;
+    }).catch((err) => {
+      if (err.stack) {
+        log.error(err.stack, err, catCLI);
+      }
+      return err;
+    });
+  });
+
+program.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
