d89b887def96238a2da72ef9f06e4f2c49b6f1a8
Benjamin Reed <ranger@opennms.org>
2018-03-07 15:16:27 -0500

    test(dao): add unit tests for flow dao

--- test/dao/FlowDAO.spec.ts
+++ test/dao/FlowDAO.spec.ts
@@ -5,6 +5,10 @@ import {Client} from '../../src/Client';
 import {OnmsAuthConfig} from '../../src/api/OnmsAuthConfig';
 import {OnmsServer} from '../../src/api/OnmsServer';
 
+import {OnmsFlowExporter} from '../../src/model/OnmsFlowExporter';
+import {OnmsFlowExporterSummary} from '../../src/model/OnmsFlowExporterSummary';
+import {OnmsFlowTable} from '../../src/model/OnmsFlowTable';
+
 import {FlowDAO} from '../../src/dao/FlowDAO';
 
 import {MockHTTP22} from '../rest/MockHTTP22';
@@ -32,6 +36,27 @@ describe('FlowDAO', () => {
             done();
         });
     });
+    it('FlowDao.getExporters(1)', () => {
+        return dao.getExporters(1).then((summaries) => {
+            expect(summaries.length).toEqual(1);
+            expect(summaries[0]).toBeInstanceOf(OnmsFlowExporterSummary);
+            expect(summaries[0].id).toEqual(69); // nice
+        });
+    });
+    it('FlowDao.getExporter(test:test-node)', () => {
+        return dao.getExporter('test:test-node', 1).then((summary) => {
+            expect(summary).toBeInstanceOf(OnmsFlowExporter);
+            expect(summary.id).toEqual(69); // nice
+        });
+    });
+    it('FlowDao.getSummaryForTopNApplications()', () => {
+        return dao.getSummaryForTopNApplications().then((summary) => {
+            expect(summary).toBeInstanceOf(OnmsFlowTable);
+            expect(summary.start).toBeInstanceOf(moment);
+            expect(summary.end).toBeInstanceOf(moment);
+            expect(summary.headers).toContain('Application');
+        });
+    });
     it('FlowDao.getSeriesForTopNApplications()', () => {
         return dao.getSeriesForTopNApplications().then((series) => {
             expect(series.start).toBeInstanceOf(moment);
@@ -40,6 +65,25 @@ describe('FlowDAO', () => {
             expect(series.timestamps.length).toEqual(49);
             expect(series.values.length).toEqual(10);
             expect(series.values[0].length).toEqual(49);
+        });
+    });
+    it('FlowDao.getSummaryForTopNConversations()', () => {
+        return dao.getSummaryForTopNConversations().then((summary) => {
+            expect(summary).toBeInstanceOf(OnmsFlowTable);
+            expect(summary.start).toBeInstanceOf(moment);
+            expect(summary.end).toBeInstanceOf(moment);
+            expect(summary.headers).toContain('Location');
+            expect(summary.rows.length).toEqual(10);
+        });
+    });
+    it('FlowDao.getSeriesForTopNConversations()', () => {
+        return dao.getSeriesForTopNConversations().then((series) => {
+            expect(series.start).toBeInstanceOf(moment);
+            expect(series.end).toBeInstanceOf(moment);
+            expect(series.columns.length).toEqual(10);
+            expect(series.timestamps.length).toEqual(2);
+            expect(series.values.length).toEqual(10);
+            expect(series.values[0].length).toEqual(2);
         });
     });
 });
