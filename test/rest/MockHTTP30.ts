a4347f61eaecbfb484d20aafa78646da9c3295b8
aramosvizcarra <aramosvizcarra@gmail.com>
2022-08-23 14:51:41 -0400

    feat(test): added some unit tests for snmpinterface dao

---
+++ test/rest/MockHTTP30.ts
@@ -0,0 +1,30 @@
+import {AbstractMockHTTP} from './AbstractMockHTTP';
+import {OnmsHTTPOptions} from '../../src/api/OnmsHTTPOptions';
+
+/** Mock OpenNMS 28.x HTTP implementation */
+export class MockHTTP30 extends AbstractMockHTTP {
+  /** @inheritdoc */
+  public onGet(url: string, options?: OnmsHTTPOptions) {
+    switch(url) {
+      case 'http://demo.opennms.org/opennms/rest/info': {
+        return this.okJson({
+          displayVersion: '30.0.0',
+          packageDescription: 'OpenNMS',
+          packageName: 'opennms',
+          version: '30.0.0',
+        });
+      }
+      // Use the v30 responses   
+      case 'api/v2/snmpinterfaces': {
+        return this.okJsonFile('./30.0.0/get/api/v2/snmpinterfaces.json');
+      } 
+      case 'api/v2/snmpinterfaces/properties': {
+        return this.okJsonFile('./30.0.0/get/api/v2/snmpinterfaces.properties.json');
+      } 
+      case 'api/v2/snmpinterfaces?limit=1000&_s=ifName%3D%3Dsome-test':{
+        return this.okJsonFile('./30.0.0/get/api/v2/snmpInterfaces.filtered.json');
+      }    
+
+    }
+  }
+}
