6aa323c663a8528f043dccd4400944f92a30c9ba
CI/CD System <cicd-system@opennms.com>
2022-08-30 15:48:33 +0000

    autogenerated runtime build artifacts

--- dist/cli.node.js
+++ dist/cli.node.js
@@ -88246,6 +88246,8 @@ var OnmsSnmpInterface = /*#__PURE__*/function () {
 
     /** the last time the SNMP interface was polled */
 
+    /** The node associated to this snmpInterface */
+
     /** @inheritdoc */
     function get() {
       return String(this.id);
@@ -88276,7 +88278,8 @@ var OnmsSnmpInterface = /*#__PURE__*/function () {
         collect: (_this$collect = this.collect) === null || _this$collect === void 0 ? void 0 : _this$collect.toJSON(),
         poll: this.poll,
         lastSnmpPoll: (_this$lastSnmpPoll = this.lastSnmpPoll) === null || _this$lastSnmpPoll === void 0 ? void 0 : _this$lastSnmpPoll.toJSON(),
-        physAddr: (_this$physAddr = this.physAddr) === null || _this$physAddr === void 0 ? void 0 : _this$physAddr.urlValue
+        physAddr: (_this$physAddr = this.physAddr) === null || _this$physAddr === void 0 ? void 0 : _this$physAddr.urlValue,
+        nodeId: this.nodeId
       };
     }
   }], [{
@@ -88296,6 +88299,7 @@ var OnmsSnmpInterface = /*#__PURE__*/function () {
       iface.collect = OnmsCollectType.forId(data.collectFlag);
       iface.poll = data.poll;
       iface.lastSnmpPoll = Util.toDate(data.lastSnmpPoll);
+      iface.nodeId = Util.toNumber(data.nodeId);
 
       if (data.physAddr) {
         iface.physAddr = new PhysAddr(data.physAddr);
