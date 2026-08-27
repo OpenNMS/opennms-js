5fd19f1748a5433e3e541034f2cfd1988219eccd
Scott Theleman <synqotik@users.noreply.github.com>
2026-08-26 23:37:02 -0400

    build: NMS-20266 disable commitlint subject-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
+    rules: {
+        // Subjects here start with a JIRA key (e.g. "NMS-20266 update ..."), which
+        // config-conventional reads as upper-case/start-case.
+        'subject-case': [0],
+    },
 };
